82ee20d4906f8ce79c1bd1873d5465eec8719924
Lorraine254 <lorraine.atsulu@gmail.com>
2024-01-17 16:09:49 +0300

    Creating a list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 //importing the message component from current folder
-import Message from './Message';
+import ListGroup from './components/ListGroup';
 
 //creating a new component
 //Using PascalCasing
 function App() {
   //Using the message component
-  return <div><Message /></div> //always close your React components
+  return <div><ListGroup/></div> //always close your React components
 }
 
 //to use the component-export it
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+//PascalCasing
+
+function ListGroup() {
+  return (
+    <div>
+        <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
+    </div>
+  );
+}
+//to use the componenet - export it as a default component
+export default ListGroup;
